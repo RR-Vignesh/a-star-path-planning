2d77da565da812533e808346cbe90704c1365c67
guruadp <gurunandhanadp@gmail.com>
2023-03-15 12:27:11 -0400

    get input

--- a-star.py
+++ a-star.py
@@ -65,4 +65,31 @@ def create_obstacle_map():
             if  (yp - (0.577)*xp - (32.692)) < 0 and (yp + (0.577)*xp - (378.846)) < 0 and (yp - (0.577)*xp + (128.846)) > 0 and (yp + (0.577)*xp - (217.307)) > 0 and (230 <= xp <= 370):
                 obstacle_points.append((xp,yp))
 
+# get input and check if valid or not
+def get_input():
+    accept_start_node, accept_goal_node = True, True
+    while accept_start_node:
+        start_x = int(input("Enter start x: "))
+        start_y = int(input("Enter start y: "))
+        start_node = (start_x, start_y)
+        if start_node not in obstacle_points:
+            accept_start_node = False
+            start_theta = int(input("Enter start theta: "))
+            start_node = (start_x, start_y, start_theta)
+        else:
+            print("Entered start node is in obstacle. Please enter a valid note...")
+
+    while accept_goal_node:    
+        goal_x = int(input("Enter goal x: "))
+        goal_y = int(input("Enter goal y: "))        
+        goal_node = (goal_x, goal_y)
+        if goal_node not in obstacle_points:
+            accept_goal_node = False
+            goal_theta = int(input("Enter goal theta: "))
+            goal_node = (goal_x, goal_y, goal_theta)
+        else:
+            print("Entered goal node is in obstacle. Please enter a valid note...")
+    return start_node, goal_node
+
 create_obstacle_map()
+start_node, goal_node = get_input()
