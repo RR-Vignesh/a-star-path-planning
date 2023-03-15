a7fc30c27b795719d2930ad158a41f47eddce8d5
guruadp <gurunandhanadp@gmail.com>
2023-03-15 17:21:54 -0400

    add visualization

--- a-star.py
+++ a-star.py
@@ -1,6 +1,7 @@
 import math 
 from queue import PriorityQueue
 import time
+import pygame as pyg
 
 obstacle_points = []
 
@@ -264,6 +265,87 @@ def back_tracking(path_info, initial, current):
         shortest_path.append(child_tuple)
     return shortest_path
 
+# to flip the coordinates because of the origin change in pygame
+def flip_points(points, height):
+    return (points[0], height - points[1])
+
+def flip_object_points(points, height, object_height):
+    return (points[0], height - points[1] - object_height)
+
+def pygame_visualization(visited_nodes, shortest_path):
+    pyg.init()
+    window = pyg.display.set_mode((X_SIZE,Y_SIZE))
+
+    obstacle_color = "red"
+    clearance_color = "pink"
+    condition = True
+    clock = pyg.time.Clock()
+
+    rect2_clearance = flip_object_points([95, 0], 250, 105)
+    rect1_clearance = flip_object_points([95, 145], 250, 105)
+    rect2_original = flip_object_points([100, 0], 250, 100)
+    rect1_original = flip_object_points([100, 150], 250, 100)
+
+    triangle1_clearance = flip_points([455, 20], 250)
+    triangle2_clearance = flip_points([463, 20], 250)
+    triangle3_clearance = flip_points([515.5, 125], 250)
+    triangle4_clearance = flip_points([463, 230], 250)
+    triangle5_clearance = flip_points([455, 230], 250)
+
+    triangle1 = (460, 25)
+    triangle2 = (460, 225)
+    triangle3 = (510, 125)
+
+    hexagon1_clearance = (300, 205.76)
+    hexagon2_clearance = (230, 165.38)
+    hexagon3_clearance = (230, 84.61)
+    hexagon4_clearance = (300, 44.23)
+    hexagon5_clearance = (370, 84.61)
+    hexagon6_clearance = (370, 165.38)
+
+    hexagon1_org = (235,87.5)
+    hexagon2_org = (300,50)
+    hexagon3_org = (365,87.5)
+    hexagon4_org = (365,162.5)
+    hexagon5_org = (300,200)
+    hexagon6_org = (235,162.5)
+
+    while condition:
+        for loop in pyg.event.get():
+            if loop.type == pyg.QUIT:
+                condition = False
+
+        pyg.draw.rect(window, clearance_color, pyg.Rect(rect2_clearance[0], rect2_clearance[1], 60, 105))
+        pyg.draw.rect(window, clearance_color, pyg.Rect(rect1_clearance[0], rect1_clearance[1], 60, 105))
+        pyg.draw.rect(window, obstacle_color, pyg.Rect(rect2_original[0], rect2_original[1], 50, 100))
+        pyg.draw.rect(window, obstacle_color, pyg.Rect(rect1_original[0], rect1_original[1], 50, 100))
+
+        pyg.draw.polygon(window, clearance_color, ((triangle1_clearance),(triangle2_clearance),(triangle3_clearance), (triangle4_clearance), (triangle5_clearance)))
+        pyg.draw.polygon(window, obstacle_color, ((triangle1),(triangle2),(triangle3)))
+
+        pyg.draw.polygon(window, clearance_color, ((hexagon1_clearance),(hexagon2_clearance),(hexagon3_clearance),(hexagon4_clearance),(hexagon5_clearance),(hexagon6_clearance)))
+        pyg.draw.polygon(window, obstacle_color, ((hexagon1_org),(hexagon2_org),(hexagon3_org),(hexagon4_org),(hexagon5_org),(hexagon6_org)))
+
+        pyg.draw.rect(window, clearance_color ,pyg.Rect(0, 0, 600, 5))
+        pyg.draw.rect(window, clearance_color ,pyg.Rect(0, 245, 600, 5))
+        pyg.draw.rect(window, clearance_color ,pyg.Rect(0, 0, 5, 250))
+        pyg.draw.rect(window, clearance_color ,pyg.Rect(595, 0, 5, 250))
+
+        for node in visited_nodes:
+            pyg.draw.circle(window, "white", flip_points(node, 250), 1)
+            pyg.display.flip()
+            clock.tick(700)
+
+        for node in shortest_path:
+            pyg.draw.circle(window, "teal", flip_points(node, 250), 1)
+            pyg.display.flip()
+            clock.tick(10)
+
+        pyg.display.flip()
+        pyg.time.wait(3000)
+        condition = False
+    pyg.quit()
+
 create_obstacle_map()
 start_node, goal_node = get_input()
 # print(eclidean_distance((0, 0), (1, 1)))
@@ -308,3 +390,5 @@ while True:
         shortest.reverse()  
         print(shortest)
         break
+
+pygame_visualization(visited_nodes, shortest_path)
